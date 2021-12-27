c2251b60129df50b3b60d20240a456fd3b716a30
pidokige02 <pidokige0204@gmail.com>
2021-12-28 06:18:22 +0900

    L2_lesson_start

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <BaseInput
+      label="Email"
+      type="email"
+      v-model="email"
+      :error="emailError"
+    />
+
+    <BaseInput
+      label="Password"
+      type="password"
+    />
+
+    <BaseButton
+      type="submit"
+      class="-fill-gradient"
+    >
+    Submit
+    </BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  setup () {
+    function onSubmit () {
+      alert('Submitted')
+    }
+
+    return {
+      onSubmit
+    }
+  }
+}
+</script>
